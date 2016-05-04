85f1d49b6e3ac4f48f1b5a83f9d908bbc397e1cc
mrchypark <mrchypark@gmail.com>
2016-05-04 21:24:05 +0900

    get dir

--- R/getUrlList.R
+++ R/getUrlList.R
@@ -5,4 +5,3 @@ getUrlList<-function(urls=urls){
   
 }
 
-file.exists("C:/Users/parkets/.ssh/id_rsa")
---
+++ R/setUrl.R
@@ -0,0 +1,13 @@
+setUrlByCategory<-function(strDate=strDate,endDate=endDate,category=category){
+  
+  
+  
+  
+}
+
+setUrlQuery<-function(strDate=strDate,endDate=endDate,query=query){
+  
+  
+  
+  
+}
