496a114c50fa7cf9ddfb46a9f4f655287377d152
mrchypark <mrchypark@gmail.com>
2016-05-08 00:18:01 +0900

    get

--- R/getCategoryUrl.R
+++ R/getCategoryUrl.R
@@ -11,25 +11,15 @@
 #' @import xml2
 #' @import rvest
 
-getCategoryUrl<-function(select=c(1,2,3,4,5,6)){
+getCategoryUrl <-function(){
 
-  home <- "http://news.naver.com/"
-  tem <- readLines(home,warn=F)
-  tem <- tem[grep("class=\"m[3-8] nclick",tem)]
-  cate_names<-xml2::read_html(paste0(tem,collapse=" ")) %>%
-    rvest::html_nodes("a") %>%
-    rvest::html_text()
-  Encoding(cate_names)<-"UTF-8"
-  cate_code<-data.frame(cate_name=cate_names,code=c(100,101,102,103,104,105))
+  cate_code<-c(100,101,102,103,104,105)
   url  <- "http://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1="
-  urls <- data.frame(cate_name=NA,cate_sub=NA,cate_url=NA)
+  urls <- data.frame(cate_name=NA,cate_url=NA)
   urls <- urls[-1,]
+  for (code in cate_code){
 
-  cate_code <- cate_code[select,]
-
-  for (code in 1:nrow(cate_code)){
-
-    category_url<-paste0(url,cate_code[code,2])
+    category_url<-paste0(url,code)
     tem <- readLines(category_url,warn=F)
     tem <- tem[grep("class=\"snb_s",tem)]
     if(!identical(grep("snb_s17",tem),integer(0))){
@@ -44,8 +34,7 @@ getCategoryUrl<-function(select=c(1,2,3,4,5,6)){
       rvest::html_nodes("a") %>%
       rvest::html_attr("href")
 
-    urls<-rbind(urls,data.frame(cate_name=cate_code[code,1],cate_sub=cate_names,cate_url=cate_urls))
+    urls<-rbind(urls,data.frame(cate_name=cate_names,cate_url=cate_urls))
   }
   return(urls)
 }
-
