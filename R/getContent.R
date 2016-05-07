b8c8b1c3d43475ed0826dfb1439dfd644e1bd2d9
mrchypark <mrchypark@gmail.com>
2016-05-08 02:42:38 +0900

    bypass moved page

--- R/getContent.R
+++ R/getContent.R
@@ -5,36 +5,45 @@
 #' @return Get data.frame(url,datetime,press,title,content).
 #' @export
 #' @import rvest
+#' @import RCurl
 
 getContent<-function(url=url){
 
-  tem <- read_html(url)
-  title <- tem %>%
-    html_nodes("div.article_info h3") %>%
-    html_text()
-  Encoding(title)<-"UTF-8"
+  if(url.exists(url)){
 
-  datetime <- tem %>%
-    html_nodes("span.t11") %>%
-    html_text()
-  datetime <- as.POSIXlt(datetime)
+    tem <- read_html(url)
+    title <- tem %>%
+      html_nodes("div.article_info h3") %>%
+      html_text()
+    Encoding(title)<-"UTF-8"
 
-  if(length(datetime)==1){edittime<-""}
-  if(length(datetime)==2){edittime<-datetime[2];datetime<-datetime[1]}
+    datetime <- tem %>%
+      html_nodes("span.t11") %>%
+      html_text()
+    datetime <- as.POSIXlt(datetime)
 
-  press <- tem %>%
-    html_nodes("div.article_header div a img") %>%
-    html_attr("title")
-  Encoding(press)<-"UTF-8"
+    if(length(datetime)==1){edittime<-""}
+    if(length(datetime)==2){edittime<-datetime[2];datetime<-datetime[1]}
 
-  content <- tem %>%
-    html_nodes("div#articleBodyContents") %>%
-    html_text()
-  Encoding(content)<-"UTF-8"
-  content <- stri_trim(content)
+    press <- tem %>%
+      html_nodes("div.article_header div a img") %>%
+      html_attr("title")
+    Encoding(press)<-"UTF-8"
 
-#  tet<-GET("https://apis.naver.com/commentBox/cbox5/web_naver_list_jsonp.json?ticket=news&templateId=view_politics&_callback=window.__cbox_jindo_callback._9023&lang=ko&country=KR&objectId=news421%2C0002040415&categoryId=&pageSize=10&indexSize=10&groupId=&page=1&initialize=true&useAltSort=true&replyPageSize=30&moveTo=&sort=&userType=")
+    content <- tem %>%
+      html_nodes("div#articleBodyContents") %>%
+      html_text()
+    Encoding(content)<-"UTF-8"
+    content <- stri_trim(content)
 
-  newsInfo <- data.frame(url=url,datetime=datetime,edittime=edittime,press=press,title=title,content=content,stringsAsFactors = F)
+  #  tet<-GET("https://apis.naver.com/commentBox/cbox5/web_naver_list_jsonp.json?ticket=news&templateId=view_politics&_callback=window.__cbox_jindo_callback._9023&lang=ko&country=KR&objectId=news421%2C0002040415&categoryId=&pageSize=10&indexSize=10&groupId=&page=1&initialize=true&useAltSort=true&replyPageSize=30&moveTo=&sort=&userType=")
+
+    newsInfo <- data.frame(url=url,datetime=datetime,edittime=edittime,press=press,title=title,content=content,stringsAsFactors = F)
+
+    }else{
+
+    newsInfo <- data.frame(url=url,datetime="page is moved.",edittime="page is moved.",press="page is moved.",title="page is moved.",content="page is moved.",stringsAsFactors = F)
+
+  }
   return(newsInfo)
 }
