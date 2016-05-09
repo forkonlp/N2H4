db06922500c838693b6d28bce5fa3e3faa6d4fc3
mrchypark <mrchypark@gmail.com>
2016-05-09 18:46:10 +0900

    update example

--- demo/example.R
+++ demo/example.R
@@ -1,3 +1,7 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load_gh("mrchypark/N2H4")
+
+
 # example using category
 
 strDate <- as.Date("2016-01-01")
@@ -6,22 +10,43 @@ endDate <- as.Date("2016-01-02")
 for (Date in strDate:endDate){
 
   urls <- setUrlByCategory(select=1,targetDate = as.Date(Date,origin= "1970-01-01"))
-  allList <- getAllUrlListByCategory(urls)
 
-  for (url in allList[,2]){
-    print(url)
-    newsData <- getContent(url)
-    filenames <- substr(newsData$url,nchar(newsData$url)-9,nchar(newsData$url))
-    dir.create("./data",showWarnings = FALSE)
-    write.csv(newsData,paste0("./data/",Date,"_",filenames,".csv"),row.names=F)
+  forurlend <- nrow(urls)
+
+  for (loc in 1:forurlend) {
+    print(paste0("start ", urls[loc, 2], " / ", urls[loc, 1]))
+    urlList <- getUrlListByCategory(urls[loc, 3])
+    compList <- c()
+    pageNum <- 2
+    print("get first page.")
+
+    for (url in urlList[,2]){
+      print(url)
+      newsData <- getContent(url)
+      filenames <- substr(newsData$url,nchar(newsData$url)-9,nchar(newsData$url))
+      dir.create("./data",showWarnings = FALSE)
+      write.csv(newsData,paste0("./data/",Date,"_",filenames,".csv"),row.names=F)
+    }
+    while (!identical(urlList, compList)) {
+      print(paste0("get ", pageNum, " page."))
+      compList <- urlList
+      url <- paste0(urls[loc, 3], "&page=", pageNum)
+      urlList <- getUrlListByCategory(url)
+      pageNum <- pageNum + 1
+      for (url in urlList[,2]){
+        print(url)
+        newsData <- getContent(url)
+        filenames <- substr(newsData$url,nchar(newsData$url)-9,nchar(newsData$url))
+        dir.create("./data",showWarnings = FALSE)
+        write.csv(newsData,paste0("./data/",Date,"_",filenames,".csv"),row.names=F)
+      }
+    }
   }
-
 }
 
 
-
-
 # example using query
+# not stable
 
 strDate <- as.Date("2016-01-01")
 endDate <- as.Date("2016-01-02")
