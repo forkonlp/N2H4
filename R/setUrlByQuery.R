a0cb4ba4b328b229e52ac46c6dd3c9bed5647081
mrchypark <mrchypark@gmail.com>
2016-08-22 12:58:56 +0900

    fix for mac, linux

--- R/setUrlByQuery.R
+++ R/setUrlByQuery.R
@@ -10,8 +10,8 @@ setUrlByQuery <- function(query = "", targetDate = as.Date("2016-01-01")) {
     
     base_url <- "http://news.naver.com/main/search/search.nhn?query="
     query_for_url <- RCurl::curlEscape(query)
-    urls <- data.frame(query_name = query, query_for_url = query_for_url, query_url = paste0(base_url, query_for_url, 
-        "&startDate=", targetDate, "&endDate=", targetDate), stringsAsFactors = F)
+    urls <- data.frame(query_name = query, query_for_url = query_for_url, query_url = paste0(base_url, query_for_url, "&startDate=", targetDate, "&endDate=", targetDate), 
+        stringsAsFactors = F)
     return(urls)
     
 }
