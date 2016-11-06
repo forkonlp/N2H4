a52570cd7dec5c75f7bff14e9579a9b1d0b02a28
mrchypark <mrchypark@gmail.com>
2016-11-06 21:16:56 +0900

    add getSid1 function

--- R/getSid1.R
+++ R/getSid1.R
@@ -1,19 +1,16 @@
-#' Get Category Url
+#' Get Main Category
 #'
-#' Get naver news sub_category names and urls recently.
-#' Main category urls are written manually like blow.
-#' cate_code<-c(100,101,102,103,104,105)
-#' There are 6 categories in never news.
+#' Get naver news main category names and urls recently.
+#' There are 7 categories in never news.
 #' 1: Politics, 2: Economics, 3: Social, 4: Living / Culture, 5: World, 6: IT / science, 7: Opinion
 #'
-#' @param select from 1 to 7 numeric values which mean categories.
-#' @return Get data.frame(cate_name, cate_sub, sid1, sid2).
+#' @return Get data.frame(chr:cate_name, chr:sid1).
 #' @export
 #' @import xml2
 #' @import rvest
 #' @import stringr
 
-getCategoryUrl <- function() {
+getSid1 <- function() {
 
   home <- "http://news.naver.com/"
   titles <- read_html(home) %>% html_nodes("a") %>% html_text()
@@ -21,8 +18,10 @@ getCategoryUrl <- function() {
   titles<-titles[grep("^\\/main\\/main.nhn\\?mode=LSD&mid=shm&sid1=1",links)]
   titles<-str_trim(titles)
   links<-links[grep("^\\/main\\/main.nhn\\?mode=LSD&mid=shm&sid1=1",links)]
-
+  sid1<-str_sub(links,-3,-1)
+  urls<-data.frame(cate_name=titles,sid1=sid1,stringsAsFactors = F)
   return(urls)
+
 }
 
 
