fcc40cc67a8ac7c3ea7cb78a26c00c9f5eba66bf
mrchypark <mrchypark@gmail.com>
2016-11-17 15:26:12 +0900

    fix min

--- R/getUrlListByQuery.R
+++ R/getUrlListByQuery.R
@@ -1,6 +1,6 @@
 #' Get Url List By Query
 #'
-#' Get naver news titles and links from target url.
+#' Get naver news(only not other sites links) titles(are just "dummy" now) and links from target url.
 #'
 #' @param url is target url naver news.
 #' @return Get data.frame(news_title, news_links).
