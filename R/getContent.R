cd8a5a817dcf13a271c0ea8a2c17fdad1d63519d
mrchypark <mrchypark@gmail.com>
2017-05-22 08:18:35 +0000

    refac content func

--- R/getContent.R
+++ R/getContent.R
@@ -4,56 +4,180 @@
 #'
 #' @param url is naver news link.
 #' @param col is what you want to get from news. Defualt is all.
-#' @return Get data.frame(url,datetime,press,title,content).
+#' @param async async crawling if it is TRUE. Defualt is FALSE.
+#' @return Get data.frame(url,datetime,press,title,body).
 #' @export
+#' @import curl
 #' @import RCurl
 #' @import xml2
 #' @import rvest
 #' @import stringr
 
-getContent <- function(url = url, col=c("url","datetime","press","title","content")) {
+getContent <- function(url = url, col=c("url","datetime","press","title","body"), async=FALSE) {
 
   if(!identical(url,character(0))){
     if (RCurl::url.exists(url)&
        "error_msg 404"!=(read_html(url)%>%html_nodes("div#main_content div div")%>%html_attr("class"))[1]
         ) {
+      urlcheck<-GET(url)$url
+      if(!identical(grep("news.naver.com",urlcheck),integer(0))){
 
-        tem <- read_html(url)
-        title <- tem %>% html_nodes("div.article_info h3") %>% html_text()
-        Encoding(title) <- "UTF-8"
+          tem <- read_html(url)
+          title<-getContentTitle(tem)
+          datetime<-getContentDatetime(tem)[1]
+          edittime<-getContentDatetime(tem)[2]
+          press<-getContentPress(tem)
+          body<-getContentBody(tem)
 
-        datetime <- tem %>% html_nodes("span.t11") %>% html_text()
-        datetime <- as.POSIXlt(datetime)
+          newsInfo <- data.frame(url = url, datetime = datetime, edittime = edittime, press = press, title = title, body = body, stringsAsFactors = F)
 
-        if (length(datetime) == 1) {
-            edittime <- datetime[1]
-        }
-        if (length(datetime) == 2) {
-            edittime <- datetime[2]
-            datetime <- datetime[1]
-        }
-
-        press <- tem %>% html_nodes("div.article_header div a img") %>% html_attr("title")
-        Encoding(press) <- "UTF-8"
-
-        content <- tem %>% html_nodes("div#articleBodyContents") %>% html_text()
-        Encoding(content) <- "UTF-8"
-        content <- str_trim(content,side="both")
-        content <- gsub("\r?\n|\r", " ", content)
-
-        newsInfo <- data.frame(url = url, datetime = datetime, edittime = edittime, press = press, title = title, content = content, stringsAsFactors = F)
-
+      } else {
+        newsInfo <- data.frame(url = url, datetime = "page is not news section.",
+                               edittime = "page is not news section.",
+                               press = "page is not news section.",
+                               title = "page is not news section.",
+                               body = "page is not news section.",
+                               stringsAsFactors = F)
+      }
+      return(newsInfo[,col])
     } else {
 
-        newsInfo <- data.frame(url = url, datetime = "page is moved.", edittime = "page is moved.", press = "page is moved.", title = "page is moved.", content = "page is moved.",
+        newsInfo <- data.frame(url = url, datetime = "page is moved.",
+                               edittime = "page is moved.",
+                               press = "page is moved.",
+                               title = "page is moved.",
+                               body = "page is moved.",
             stringsAsFactors = F)
 
     }
     return(newsInfo[,col])
   } else { print("no news links")
 
-    newsInfo <- data.frame(url = "no news links", datetime = "no news links", edittime = "no news links", press = "no news links", title = "no news links", content = "no news links",
+    newsInfo <- data.frame(url = "no news links",
+                           datetime = "no news links",
+                           edittime = "no news links",
+                           press = "no news links",
+                           title = "no news links",
+                           body = "no news links",
                            stringsAsFactors = F)
     return(newsInfo[,col])
-    }
+  }
 }
+
+
+
+#' Get Content Title
+#'
+#' Get naver news Title from link.
+#'
+#' @param html_obj "xml_document" "xml_node" using read_html function.
+#' @param node_info Information about node names like tag with class or id. Default is "div.article_info h3" for naver news title.
+#' @return Get character title.
+#' @export
+#' @import xml2
+#' @import rvest
+
+getContentTitle<-function(html_ojb=tem, node_info="div.article_info h3", attr=""){
+  if(attr!=""){title <- html_ojb %>% html_nodes(node_info) %>% html_attr(attr)}else{
+  title <- html_ojb %>% html_nodes(node_info) %>% html_text()}
+  Encoding(title) <- "UTF-8"
+  return(title)
+}
+
+
+#' Get Content datetime
+#'
+#' Get naver news published datetime from link.
+#'
+#' @param html_obj "xml_document" "xml_node" using read_html function.
+#' @param node_info Information about node names like tag with class or id. Default is "div.article_info h3" for naver news title.
+#' @param getEdittime if TRUE, can get POSIXlt type datetime length 2 means published time and final edited time. if FALSE, get Date length 1.
+#' @return Get POSIXlt type datetime.
+#' @export
+#' @import xml2
+#' @import rvest
+
+getContentDatetime<-function(html_ojb=tem, node_info="span.t11", attr="", getEdittime=TRUE){
+  if(attr!=""){datetime <- html_ojb %>% html_nodes(node_info) %>% html_attr(attr)}else{
+    datetime <- html_ojb %>% html_nodes(node_info) %>% html_text()}
+  datetime <- as.POSIXlt(datetime)
+
+  if(getedittime){
+    if (length(datetime) == 1) {
+      edittime <- datetime[1]
+    }
+    if (length(datetime) == 2) {
+      edittime <- datetime[2]
+      datetime <- datetime[1]
+    }
+    datetime<-c(datetime,edittime)
+    return(datetime)
+  }
+  return(datetime)
+}
+
+
+#' Get Content Press name.
+#'
+#' Get naver news press name from link.
+#'
+#' @param html_obj "xml_document" "xml_node" using read_html function.
+#' @param node_info Information about node names like tag with class or id. Default is "div.article_info h3" for naver news title.
+#' @return Get character press.
+#' @export
+#' @import xml2
+#' @import rvest
+
+getContentPress<-function(html_ojb=tem, node_info="div.article_header div a img", attr="title"){
+  if(attr!=""){press <- html_ojb %>% html_nodes(node_info) %>% html_attr(attr)}else{
+    press <- html_ojb %>% html_nodes(node_info) %>% html_text()}
+  Encoding(press) <- "UTF-8"
+  return(press)
+}
+
+
+
+#' Get Content body name.
+#'
+#' Get naver news body from link.
+#'
+#' @param html_obj "xml_document" "xml_node" using read_html function.
+#' @param node_info Information about node names like tag with class or id. Default is "div.article_info h3" for naver news title.
+#' @return Get character body content.
+#' @export
+#' @import xml2
+#' @import rvest
+#' @import stingr
+
+getContentBody<-function(html_ojb=tem, node_info="div#articleBodyContents", attr=""){
+  if(attr!=""){body <- html_ojb %>% html_nodes(node_info) %>% html_attr(attr)}else{
+    body <- html_ojb %>% html_nodes(node_info) %>% html_text()}
+  Encoding(body) <- "UTF-8"
+  content <- str_trim(content,side="both")
+  content <- gsub("\r?\n|\r", " ", content)
+  return(body)
+}
+
+
+
+
+#
+# tem<-getUrlListByCategory("http://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid1=101&sid2=258")
+#
+# pool <- new_pool()
+#
+# data <- list()
+# success <- function(res){
+#   cat("Request done! Status:", res$status, "\n")
+#   res$content<-iconv(rawToChar(res$content),from="CP949",to="UTF-8")
+#   data <<- c(data, list(res))
+# }
+# failure <- function(msg){
+#   cat("Oh noes! Request failed!", msg, "\n")
+# }
+#
+# sapply(tem$links, function(x) curl_fetch_multi(x,success,failure))
+#
+# multi_run()
+# str(data)
+
