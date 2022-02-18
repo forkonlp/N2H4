958cac9ccace860384882847a6c3971bcdb31c3f
Chanyub.Park <mrchypark@gmail.com>
2022-02-18 13:58:54 +0000

    add nowire testset using httptest package

--- tests/testthat/setup-N2H4.R
+++
@@ -1,5 +0,0 @@
-library("vcr") # *Required* as vcr is set up on loading
-invisible(vcr::vcr_configure(
-  dir = vcr::vcr_test_path("fixtures")
-))
-vcr::check_cassette_names()
--- tests/testthat/test-func.R
+++ tests/testthat/test-func.R
@@ -1,62 +1,50 @@
 context("function test")
-
-vcr::use_cassette("Comment", {
+with_mock_api({
   test_that("getComment", {
     url  <-
-      "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009205077"
+      "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009205077"
     test <- getComment(url, type = "list")
     test <- test$result$commentList[[1]]
     expect_equal(test$contents, "test")
   })
-})
 
-vcr::use_cassette("AllComment", {
   test_that("getAllComment", {
     url  <-
-      "https://news.naver.com/main/read.nhn?mode=LS2D&mid=shm&sid1=100&sid2=264&oid=015&aid=0002303155"
+      "https://news.naver.com/main/read.naver?mode=LS2D&mid=shm&sid1=100&sid2=264&oid=015&aid=0002303155"
     test <- getAllComment(url)
     expect_equal(test$contents, c("test", "test2", "test"))
   })
-})
 
-vcr::use_cassette("Content", {
+
   test_that("getContent", {
     url  <-
-      "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009205077"
+      "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=001&aid=0009205077"
     test <- getContent(url)
     expect_equal(test$url, url)
   })
-})
 
-vcr::use_cassette("Sportsnews", {
   test_that("passSportsnews", {
     url  <-
-      "https://sports.news.naver.com/wfootball/news/read.nhn?oid=477&aid=0000073768"
+      "https://sports.news.naver.com/wfootball/news/read.naver?oid=477&aid=0000073768"
     test <- getContent(url)
     expect_equal(test$body, "page is not news section.")
   })
-})
 
-vcr::use_cassette("Main", {
   test_that("getMainCategory", {
     test <- getMainCategory()
     expect_equal(test$sid1, c("100", "101", "102", "103", "105", "104"))
   })
-})
 
-vcr::use_cassette("PageNum", {
   test_that("getMaxPageNum", {
     url  <-
-      "https://news.naver.com/main/list.nhn?sid2=254&sid1=102&mid=shm&mode=LS2D&date=20170427"
+      "https://news.naver.com/main/list.naver?sid2=254&sid1=102&mid=shm&mode=LS2D&date=20170427"
     test <- getMaxPageNum(url)
     expect_equal(test, 1)
   })
-})
 
-vcr::use_cassette("urllist", {
   test_that("getUrlList", {
     url <-
-      "https://news.naver.com/main/list.nhn?sid2=267&sid1=100&mid=shm&mode=LS2D&date=20170101"
+      "https://news.naver.com/main/list.naver?sid2=267&sid1=100&mid=shm&mode=LS2D&date=20170101"
     test <- getUrlList(url)
     expect_identical(dim(test), c(20L, 2L))
   })
